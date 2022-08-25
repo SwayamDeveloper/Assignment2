16a09d30d724572d2a2c5be4ba3963e13cddab13
swaya <swaya@DESKTOP-H27PH04>
2022-08-25 23:26:22 +0530

    push and commit

---
+++ Assignment/src/VowelsString.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class VowelsString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        System.out.print("input the string:");
+        String str = sc.nextLine();
+
+        
+        System.out.print("Number of vowels in the string: "+ count_Vowels(str)+"\n");
+        
+        }
+	
+	 public static int count_Vowels(String str)
+	    {
+	        int count = 0;
+	        for (int i = 0; i < str.length(); i++)
+	        {
+	            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i'
+	                    || str.charAt(i) == 'o' || str.charAt(i) == 'u')
+	            {
+	                count++;
+	            }
+	        }
+	        return count;
+	
+	}
+}
+
